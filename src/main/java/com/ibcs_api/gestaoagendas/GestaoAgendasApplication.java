09a351fb9a7cd8e3cd24227ccd962a449878b292
Felipe Rocha <felperok20@hotmail.com>
2022-11-15 00:23:56 -0300

    Criando projeto

---
+++ src/main/java/com/ibcs_api/gestaoagendas/GestaoAgendasApplication.java
@@ -0,0 +1,13 @@
+package com.ibcs_api.gestaoagendas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestaoAgendasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestaoAgendasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ibcs_api/gestaoagendas/GestaoAgendasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ibcs_api.gestaoagendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestaoAgendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
